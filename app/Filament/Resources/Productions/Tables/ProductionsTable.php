8e9a47d8c4b506354f776755de786b2bd2bfb1d8
Rizal Mamluatul <rizalmamluatul@Rizals-MacBook-Air.local>
2025-09-24 20:04:00 +0700

    240925 2003

--- app/Filament/Resources/Productions/Tables/ProductionsTable.php
+++ app/Filament/Resources/Productions/Tables/ProductionsTable.php
@@ -7,9 +7,13 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use pxlrbt\FilamentExcel\Actions\ExportBulkAction;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ProductionsTable
 {
@@ -31,24 +35,36 @@ class ProductionsTable
                     ->label('Total Porsi')
                     ->wrapHeader()
                     ->numeric()
+                    ->summarize([
+                        Sum::make()
+                    ])
                     ->sortable(),
                 TextColumn::make('menu_portions_sum_total_budget_cost')
                     ->sum('menuPortions', 'total_budget_cost')
                     ->label('Total Biaya Anggaran')
                     ->wrapHeader()
                     ->numeric()
+                    ->summarize([
+                        Sum::make()
+                    ])
                     ->sortable(),
                 TextColumn::make('material_requests_sum_total_estimated_cost')
                     ->label('Total Biaya Perkiraan')
                     ->sum('materialRequests', 'total_estimated_cost')
                     ->wrapHeader()
                     ->numeric()
+                    ->summarize([
+                        Sum::make()
+                    ])
                     ->sortable(),
                 TextColumn::make('material_requests_sum_total_actual_cost')
                     ->label('Total Biaya Aktual')
                     ->sum('materialRequests', 'total_actual_cost')
                     ->wrapHeader()
                     ->numeric()
+                    ->summarize([
+                        Sum::make()
+                    ])
                     ->sortable(),
                 TextColumn::make('deleted_at')
                     ->dateTime()
@@ -74,6 +90,19 @@ class ProductionsTable
                     DeleteBulkAction::make(),
                     ForceDeleteBulkAction::make(),
                     RestoreBulkAction::make(),
+                    ExportBulkAction::make()->exports([
+                        ExcelExport::make()->fromTable()
+                    ])
+                    /* ExportBulkAction::make()->exports([
+                        ExcelExport::make()->withColumns([
+                            Column::make('Kode Produksi'),
+                            Column::make('Tgl Produksi'),
+                            Column::make('Total Porsi'),
+                            Column::make('Total Biaya Anggaran'),
+                            Column::make('Total Biaya Perkiraan'),
+                            Column::make('Total Biaya Aktual')
+                        ])
+                    ]) */
                 ]),
             ]);
     }
